bb22103f42ee273ffd8311ea0cc2723a06b4e939
cry1s <cry1s@ya.ru>
2023-06-10 22:59:25 +0300

    not only .env

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ use dotenvy_macro::dotenv;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let vk_group_token = dotenv!("VK_GROUP_TOKEN").to_string();
-    let vk_user_token = dotenv!("VK_USER_TOKEN").to_string();
+    let vk_group_token = env::var("VK_GROUP_TOKEN").unwrap_or(dotenv!("VK_GROUP_TOKEN").to_string());
+    let vk_user_token = env::var("VK_USER_TOKEN").unwrap_or(dotenv!("VK_USER_TOKEN").to_string());
     let config = p4nimau_rust::Config::new(vk_group_token, vk_user_token);
     p4nimau_rust::run(config).await
 }
